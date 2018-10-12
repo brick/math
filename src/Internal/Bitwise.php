23c33489f6d59bc7408bdccf0cd5474c7b27f16b
Benjamin Morel <benjamin.morel@gmail.com>
2018-10-12 15:58:55 +0200

    Bitwise implementation operating on whole bytes

--- src/Internal/Bitwise.php
+++ src/Internal/Bitwise.php
@@ -24,78 +24,74 @@ abstract class Bitwise
      */
     public static function bitwise(BigInteger $x, BigInteger $y, string $operator) : BigInteger
     {
-        $x2 = $x->abs()->toBase(2);
-        $y2 = $y->abs()->toBase(2);
+        $bx = self::toBinary($x);
+        $by = self::toBinary($y);
 
-        // prefix a 0 to ensure than when applying the two's complement, length in bits will remain the same
-        $x2 = '0' . $x2;
-        $y2 = '0' . $y2;
+        $lx = strlen($bx);
+        $ly = strlen($by);
 
-        $lx = strlen($x2);
-        $ly = strlen($y2);
-
-        $length = max($lx, $ly);
-
-        if ($lx < $length) {
-            $x2 = str_repeat('0', $length - strlen($x2)) . $x2;
-        }
-
-        if ($ly < $length) {
-            $y2 = str_repeat('0', $length - strlen($y2)) . $y2;
+        if ($lx > $ly) {
+            $by = str_repeat("\x00", $lx - $ly) . $by;
+        } elseif ($ly > $lx) {
+            $bx = str_repeat("\x00", $ly - $lx) . $bx;
         }
 
         if ($x->isNegative()) {
-            $x2 = self::twosComplement($x2);
+            $bx = self::twosComplement($bx);
         }
-
         if ($y->isNegative()) {
-            $y2 = self::twosComplement($y2);
+            $by = self::twosComplement($by);
         }
 
-        $value = '';
+        switch ($operator) {
+            case 'and':
+                $value = $bx & $by;
+                $negative = ($x->isNegative() and $y->isNegative());
+                break;
 
-        for ($i = 0; $i < $length; $i++) {
-            $bx = (int) $x2[$i];
-            $by = (int) $y2[$i];
+            case 'or':
+                $value = $bx | $by;
+                $negative = ($x->isNegative() or $y->isNegative());
+                break;
 
-            switch ($operator) {
-                case 'and':
-                    $value .= (string) ($bx & $by);
-                    break;
-
-                case 'or':
-                    $value .= (string) ($bx | $by);
-                    break;
-
-                case 'xor':
-                    $value .= (string) ($bx ^ $by);
-                    break;
-            }
+            case 'xor':
+                $value = $bx ^ $by;
+                $negative = ($x->isNegative() xor $y->isNegative());
+                break;
         }
 
-        if ($value[0] === '1') {
-            return BigInteger::parse(self::twosComplement($value), 2)->negated();
+        if ($negative) {
+            $value = self::twosComplement($value);
         }
 
-        return BigInteger::parse($value, 2);
+        $result = self::toBigInteger($value);
+
+        return $negative ? $result->negated() : $result;
     }
 
     /**
-     * @param string $number
+     * @param string $number A positive, binary number.
      *
      * @return string
      */
     private static function twosComplement(string $number) : string
     {
-        $length = strlen($number);
+        $xor = str_repeat("\xff", strlen($number));
 
-        for ($i = 0; $i < $length; $i++) {
-            $number[$i] = ($number[$i] === '0' ? '1' : '0');
+        $number = $number ^ $xor;
+
+        for ($i = strlen($number) - 1; $i >= 0; $i--) {
+            $byte = ord($number[$i]);
+
+            if (++$byte !== 256) {
+                $number[$i] = chr($byte);
+                break;
+            }
+
+            $number[$i] = chr(0);
         }
 
-        $result = BigInteger::parse($number, 2)->plus(1)->toBase(2);
-
-        return $result;
+        return $number;
     }
 
     /**
@@ -107,30 +103,26 @@ abstract class Bitwise
      */
     private static function toBinary(BigInteger $x) : string
     {
-        $isNegative = $x->isNegative();
-        $abs = $x->abs();
-        $base = BigInteger::of(256);
-        $bytes = '';
-        $lastByte = true;
+        $calculator = Calculator::get();
 
-        while ($abs->isGreaterThanOrEqualTo($base)) {
-            list($abs, $rest) = $abs->quotientAndRemainder($base);
-            $bytes = chr(
-                $isNegative ?
-                    ($lastByte ? 256 : 255) - $rest->toInt()
-                    : $rest->toInt()
-            ) . $bytes;
-            $lastByte = false;
+        $value = (string) $x->abs();
+
+        $result = '';
+
+        while ($value !== '0') {
+            [$value, $remainder] = $calculator->divQR($value, '256');
+            $remainder = (int) $remainder;
+
+            $result .= chr($remainder);
         }
 
-        $bytes = chr(
-            $isNegative ?
-                ($lastByte ? 256 : 255) - $abs->toInt()
-                : $abs->toInt()
-        ) . $bytes;
-        $bytes = ($isNegative ? "\xff" : "\x0") . $bytes;
+        return strrev($result);
 
-        return $bytes;
+        if ($x->isNegative()) {
+            $result = self::twosComplement($result);
+        }
+
+        return $result;
     }
 
     /**
@@ -142,23 +134,26 @@ abstract class Bitwise
      */
     private static function toBigInteger(string $bytes) : BigInteger
     {
-        $isNegative = $bytes[0] === "\xff";
-        $base = BigInteger::of(256);
-        $length = strlen($bytes);
-        $value = BigInteger::zero();
+        $calculator = Calculator::get();
 
-        for ($i = 1; $i < $length; ++$i) {
-            $lastByte = ($i === $length - 1);
-            $multiplier = $base->power($length - $i - 1);
-            $value = $value->plus(
-                BigInteger::of(
-                    $isNegative
-                        ? ($lastByte ? 256 : 255) - ord($bytes[$i])
-                        : ord($bytes[$i])
-                )->multipliedBy($multiplier)
-            );
+        $result = '0';
+        $power = '1';
+
+        for ($i = strlen($bytes) - 1; $i >= 0; $i--) {
+            $index = ord($bytes[$i]);
+
+            if ($index !== 0) {
+                $result = $calculator->add($result, ($index === 1)
+                    ? $power
+                    : $calculator->mul($power, (string) $index)
+                );
+            }
+
+            if ($i !== 0) {
+                $power = $calculator->mul($power, '256');
+            }
         }
 
-        return $isNegative ? $value->negated() : $value;
+        return BigInteger::of($result);
     }
 }
