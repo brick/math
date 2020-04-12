917027e53a93419bf65be6986c29fb1ec0d9c472
Roger Vilà <rogervila@me.com>
2020-04-12 11:42:09 +0200

    Update BigDecimal.php

--- src/BigDecimal.php
+++ src/BigDecimal.php
@@ -728,9 +728,15 @@ final class BigDecimal extends BigNumber
     /**
      * {@inheritdoc}
      */
-    public function toFloat() : float
+    public function toFloat(int $decimals = 0) : float
     {
-        return (float) (string) $this;
+        $float = (float) (string) $this;
+
+        if ($decimals > 0) {
+            return number_format($float, $decimals);
+        }
+
+        return $float;
     }
 
     /**
