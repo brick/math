cf300367f8af2af1d05c2ea547c3896963df4a96
Benjamin Morel <benjamin.morel@gmail.com>
2022-08-01 23:28:15 +0200

    Return tuple instead of using references in Calculator::gcdExtended()

--- src/Internal/Calculator.php
+++ src/Internal/Calculator.php
@@ -281,9 +281,7 @@ abstract class Calculator
             $modVal = $this->mod($x, $m);
         }
 
-        $x = '0';
-        $y = '0';
-        $g = $this->gcdExtended($modVal, $m, $x, $y);
+        [$g, $x] = $this->gcdExtended($modVal, $m);
 
         if ($g !== '1') {
             return null;
@@ -327,24 +325,21 @@ abstract class Calculator
         return $this->gcd($b, $this->divR($a, $b));
     }
 
-    private function gcdExtended(string $a, string $b, string &$x, string &$y) : string
+    /**
+     * @return array{string, string, string} GCD, X, Y
+     */
+    private function gcdExtended(string $a, string $b) : array
     {
         if ($a === '0') {
-            $x = '0';
-            $y = '1';
-
-            return $b;
+            return [$b, '0', '1'];
         }
 
-        $x1 = '0';
-        $y1 = '0';
-
-        $gcd = $this->gcdExtended($this->mod($b, $a), $a, $x1, $y1);
+        [$gcd, $x1, $y1] = $this->gcdExtended($this->mod($b, $a), $a);
 
         $x = $this->sub($y1, $this->mul($this->divQ($b, $a), $x1));
         $y = $x1;
 
-        return $gcd;
+        return [$gcd, $x, $y];
     }
 
     /**
